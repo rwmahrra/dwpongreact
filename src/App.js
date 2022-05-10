6a12eaced728dbdbf3cbccbc8a7f00b1c51d3186
rwmahrra <49766063+rwmahrra@users.noreply.github.com>
2022-05-10 09:46:41 -0500

    Models and game logic
    added the blender file containing models and added some game state feedback in to the GUI

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import React, {useState, Suspense, useEffect} from 'react'
 import { Canvas } from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei"; // can be commented in for debugging
+import { KernelSize } from 'postprocessing'
+import {EffectComposer, Bloom} from "@react-three/postprocessing"
 
 import PongGame from './PongGame';
-// import GameData from './GameData';
+import GameData from './GameData';
 import Stage from "./3DModels/Stage.js"
 import PlayArea from "./3DModels/PlayArea.js"
 
@@ -29,7 +31,7 @@ function App() {
 
 
   return (
-    <Canvas className='App' mode="concurrent" camera={{position: [0,7,3.5]}}>
+    <Canvas className='App' mode="concurrent" camera={{position: [0,5.5,3.2]}}>
       {/* Add this line back into enable camera controls */}
       <OrbitControls></OrbitControls>
 
@@ -39,7 +41,7 @@ function App() {
 
       <TransparentPlane position={[0,-1,0]} size={[100,100,100]} color={"black"} opacity={0.3} />
       <TransparentPlane position={[0,-0.6,0]} size={[100,100,100]} color={"black"} opacity={0.3} />
-      {/* <GameData /> */}
+      <GameData />
       {/* Load our 3d files here */}
       <Suspense fallback={null} >
         <PlayArea position={[0,.6,0]} />
@@ -47,17 +49,22 @@ function App() {
         <Stage position={[-11.25, 0, 0]}/> 
         <Stage position={[0, 0, 0]}/> 
         <Stage position={[11.25, 0, 0]}/> 
-        <Stage position={[-11.25, 0, -27]}/> 
+        {/* <Stage position={[-11.25, 0, -27]}/> 
         <Stage position={[0, 0, -27]}/> 
-        <Stage position={[11.25, 0, -27]}/> 
+        <Stage position={[11.25, 0, -27]}/>  */}
         <Stage position={[-11.25, 0, -13.5]}/> 
         <Stage position={[0, 0, -13.5]}/> 
         <Stage position={[11.25, 0, -13.5]}/> 
-        <Stage position={[-11.25, 0, 13.5]}/> 
+        {/* <Stage position={[-11.25, 0, 13.5]}/> 
         <Stage position={[0, 0, 13.5]}/> 
-        <Stage position={[11.25, 0, 13.5]}/>  
+        <Stage position={[11.25, 0, 13.5]}/>   */}
 
         <PongGame />
+
+        {/* <EffectComposer multisampling={8}>
+          <Bloom kernelSize={3} luminanceThreshold={0} luminanceSmoothing={1} intensity={0.1} />
+          <Bloom kernelSize={KernelSize.HUGE} luminanceThreshold={0} luminanceSmoothing={0} intensity={0.1} /> 
+        </EffectComposer> */}
       
       </Suspense>
     </Canvas>
--- src/GameData.js
+++ src/GameData.js
@@ -1,86 +1,127 @@
-// // keep level and score count
+// keep level and score count
 
-// import React, {useEffect, useState} from 'react'
-// import {Canvas, useFrame} from '@react-three/fiber'
+import React, {useEffect, useState} from 'react'
+import {Canvas, useFrame} from '@react-three/fiber'
 
-// import Text from './3DComponents/Text';
+import Text from './3DComponents/Text';
+import TransparentPlane from './3DComponents/TransparentPlane';
+import { Group } from 'three';
 
 
 
-// const mqtt = require('mqtt')
-// const client = mqtt.connect("ws://127.0.0.1:9001");
-// console.log("Creating UI connections");
-// client.on('connect', function () {
+const mqtt = require('mqtt')
+const client = mqtt.connect("ws://127.0.0.1:9001");
+console.log("Creating UI connections");
+client.on('connect', function () {
  
-//   client.subscribe('player1/score', function (err) {
-//     if (!err) {
-//       console.log("connection score")
-//     }
-//   })
+  client.subscribe('player1/score', function (err) {
+    if (!err) {
+      console.log("connection score")
+    }
+  })
 
-//   client.subscribe('player2/score', function (err) {
-//     if (!err) {
-//       console.log("connection score2")
-//     }
-//   })
+  client.subscribe('player2/score', function (err) {
+    if (!err) {
+      console.log("connection score2")
+    }
+  })
 
-//   client.subscribe('game/level', function (err) {
-//     if (!err) {
-//       console.log("connection level")
-//     }
-//   })
-// });
+  client.subscribe('game/level', function (err) {
+    if (!err) {
+      console.log("connection level")
+    }
+  })
+
+  client.subscribe('game/state', function (err) {
+      if(!err) {
+          console.log("connection game state")
+      }
+  })
+});
 
 
-// export default function GameData(props) {
+export default function GameData(props) {
 
-//     const [player1Score, setPlayer1Score] = useState(0);
-//     const [player2Score, setPlayer2Score] = useState(0);
-//     const [level, setLevel] = useState(0)
+    const [player1Score, setPlayer1Score] = useState(0);
+    const [player2Score, setPlayer2Score] = useState(0);
+    const [level, setLevel] = useState(0)
+    const [currentState, setCurrentState] = useState(2);
 
-//     useEffect(() => {
+    useEffect(() => {
         
-//         client.on('message', function (topic, message) {
-//             // message is Buffer
-//             // console.log(topic)
-//             // console.log(message.toString())
+        client.on('message', function (topic, message) {
+            // message is Buffer
+            // console.log(topic)
+            // console.log(message.toString())
         
     
-//             const data = JSON.parse(message.toString());
-//             switch (topic) {
-//             case "player1/score":
-//                 let score1 = data.score;
-//                 if (score1 != player1Score) {
-//                     setPlayer1Score(score1)
-//                 }
-//                 break;
-//             case "player2/score":
-//                 let score2 = data.score;
-//                 if (score2 != player2Score) {
-//                     setPlayer2Score(score2)
-//                 }
-//                 break;
-//             case "game/level":
-//                 let levelNumber = data.level;
-//                 if (levelNumber != level) {
-//                     setLevel(levelNumber)
-//                 }
-//                 break;
-//             }
-//         })
-//     },[])
+            const data = JSON.parse(message.toString());
+            switch (topic) {
+            case "player1/score":
+                let score1 = data.score;
+                if (score1 != player1Score) {
+                    setPlayer1Score(score1)
+                }
+                break;
+            case "player2/score":
+                let score2 = data.score;
+                if (score2 != player2Score) {
+                    setPlayer2Score(score2)
+                }
+                break;
+            case "game/level":
+                let levelNumber = data.level;
+                if (levelNumber != level) {
+                    setLevel(levelNumber)
+                }
+                break;
+            case "game/state":
+                let newState = data.state;
+                if(newState != currentState) {
+                    setCurrentState(newState)
+                }
+            }
+        })
+    },[])
+
+    const userPrompt = () => {
+        // feedback based on game state 0 - waiting, 1 - ready, will start soon, 2 - running
+        switch(currentState){
+            case 0:
+                return <group>
+                        <Text position={[-4,2,2]} rotation={[-Math.PI/4, 0, 0]} text={"Enter To Play"} color={"white"} />
+                        <TransparentPlane position={[0,1,0]} size={[100,100,100]} color={"black"} opacity={0.7} />
+                    </group>
+                break;
+            case 1:
+                return <group>
+                    <Text position={[-3,2,2]} rotation={[-Math.PI/4, 0, 0]} text={"Get Ready!"} color={"white"} />
+                    <TransparentPlane position={[0,1,0]} size={[100,100,100]} color={"black"} opacity={0.4} />
+                    </group>
+                break;
+            case 2:
+                return <Text position={[-2,1,-3]} rotation={[-Math.PI/4, 0, 0]} text={"Level:" + level} color={"white"} />
+                break;
+        }
+    }
+
     
   
 
-//     return (
-//         <group position={[0,0,-1]}>
-//             <Text position={[-2,1,-6]} rotation={[-Math.PI/4, 0, 0]} text={"Level:" + level} color={"white"} />
+    return (
+        <group position={[0,0,-1]}>
 
-//             {/* player score */}
-//             {/* These are sized and rotated by best estimate, if we continue with this script, we should use lookat() or another method to create a billboard object */}
-//             <Text position={[.5,.3,-4]} rotation={[-Math.PI/4, -Math.PI/16, 0]} text={"Score:" + player1Score} color={"deepskyblue"} />
-//             {/* AI score */}
-//             <Text position={[-4.8,1,-3.2]} rotation={[-Math.PI/4, Math.PI/16, 0]} text={"Score:" + player2Score} color={"red"} />
-//         </group>
-//     )
-// }
+            
+
+            {/* Generate Gameplay messages to start: Waiting, Ready, GO! */}
+            {userPrompt()}
+            
+
+            {/* player score */}
+            {/* These are sized and rotated by best estimate, if we continue with this script, we should use lookat() or another method to create a billboard object */}
+            <Text position={[-0.4,-0.4,3.5]} rotation={[-Math.PI/2, 0, 0]} text={"" + player1Score} color={"deepskyblue"} />
+            {/* AI score */}
+            <Text position={[-0.4,-0.4,-0.5]} rotation={[-Math.PI/2, 0, 0]} text={"" + player2Score} color={"red"} />
+        </group>
+    )
+}
